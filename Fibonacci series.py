ba604086fb6c00267e73aa5232fdaf63ab994ca5
santhosh743294 <112242567+santhosh743294@users.noreply.github.com>
2022-09-26 09:51:33 +0530

    Create Fibonacci series.py

---
+++ Fibonacci series.py
@@ -0,0 +1,16 @@
+terms = int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+if nterms <= 0:
+   print("enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci series upto", nterms, ":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
