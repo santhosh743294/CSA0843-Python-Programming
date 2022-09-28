8ac5e5684340c336baa7f18bda9f0aad82616af0
santhosh743294 <112242567+santhosh743294@users.noreply.github.com>
2022-09-28 08:25:48 +0530

    Create Grade Determinination.py

---
+++ Grade Determinination.py
@@ -0,0 +1,13 @@
+a=int(input("enter the marks:"))
+if(a>=90):
+    print("grade is",s)
+elif(a>=80):
+    print("grade is",A)
+elif(a>=70):
+    print("grade is",B)
+elif(a>=60):
+    print("grade is",C)
+elif(a>=50):
+    print("grade is",D)
+else:
+    print("student got failed")
